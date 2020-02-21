9f0fadc18980be455f383c477707805774b83993
Christian Bremer <bremersee@googlemail.com>
2020-02-21 16:02:39 +0100

    Components.

--- src/app/categories/delete-category/delete-category.component.ts
+++ src/app/categories/delete-category/delete-category.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {CategorySpecification} from '../../shared/model/category-specification';
+import {ActivatedRoute, Router} from '@angular/router';
+import {SnackbarService} from '../../shared/snackbar/snackbar.service';
+import {CategoryService} from '../../shared/service/category.service';
 
 @Component({
   selector: 'app-delete-category',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteCategoryComponent implements OnInit {
 
-  constructor() { }
+  private id: string;
+
+  category: Observable<CategorySpecification>;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private snackbar: SnackbarService,
+              private categoryService: CategoryService) {
+  }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      this.id = paramMap.get('id') || '';
+      this.category = this.categoryService.getCategory(this.id);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/categories']);
+  }
+
+  deleteCategory(): void {
+    this.categoryService.deleteCategory(this.id)
+    .subscribe(() => {
+      this.router.navigate(['/categories'])
+      .then(() => this.snackbar.show('Category successfully deleted.'));
+    });
   }
 
 }
--- src/app/links/delete-link/delete-link.component.ts
+++ src/app/links/delete-link/delete-link.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import {CategorySpecification} from '../../shared/model/category-specification';
+import {ActivatedRoute, Router} from '@angular/router';
+import {SnackbarService} from '../../shared/snackbar/snackbar.service';
+import {CategoryService} from '../../shared/service/category.service';
+import {LinkSpecification} from '../../shared/model/link-specification';
+import {LinkService} from '../../shared/service/link.service';
 
 @Component({
   selector: 'app-delete-link',
@@ -7,9 +14,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteLinkComponent implements OnInit {
 
-  constructor() { }
+  private id: string;
+
+  link: Observable<LinkSpecification>;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private snackbar: SnackbarService,
+              private linkService: LinkService) {
+  }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      this.id = paramMap.get('id') || '';
+      this.link = this.linkService.getLink(this.id);
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/links']);
+  }
+
+  deleteLink(): void {
+    this.linkService.deleteLink(this.id)
+    .subscribe(() => {
+      this.router.navigate(['/links'])
+      .then(() => this.snackbar.show('Link successfully deleted.'));
+    });
   }
 
 }
