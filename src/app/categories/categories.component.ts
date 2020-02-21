0ed3f1912deaeb077ff11719861e2e2e8cef96af
Christian Bremer <bremerseegooglemail.com>
2020-02-21 08:43:49 +0100

    Adds components.

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {CategorySpecification} from '../shared/model/category-specification';
 import {CategoryService} from '../shared/service/category.service';
+import {faEdit, faTrashAlt} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-categories',
@@ -11,6 +12,10 @@ import {CategoryService} from '../shared/service/category.service';
 export class CategoriesComponent implements OnInit {
 
   categories: Observable<Array<CategorySpecification>>;
+
+  editIcon = faEdit;
+
+  deleteIcon = faTrashAlt;
 
   constructor(private categoryService: CategoryService) {
   }
