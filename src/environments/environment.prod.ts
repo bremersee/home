2bc9afbc1f8a5a82cb0326f53ee7b0f42db03fef
Christian Bremer <bremersee@googlemail.com>
2020-07-23 16:07:45 +0200

    Adds image upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  keycloakConfigLocation: '/home/assets/keycloak.json',
+  keycloakConfigLocation: '/home/assets/keycloak-prod.json',
   keycloakLogoutLocation: '/home',
   silentCheckSsoLocation: '/home/silent-check-sso.html',
   linkmanBaseUrl: 'https://api.dev.bremersee.org/linkman',
